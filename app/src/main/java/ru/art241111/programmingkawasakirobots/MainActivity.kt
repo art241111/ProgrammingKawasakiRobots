991183072ee67a61ec19a80b1f8d0066e1c948d2
art241111 <art241111@mail.ru>
2020-11-01 14:53:05 +0300

    Code refactoring

--- app/src/main/java/ru/art241111/programmingkawasakirobots/MainActivity.kt
+++ app/src/main/java/ru/art241111/programmingkawasakirobots/MainActivity.kt
@@ -43,51 +43,11 @@ class MainActivity : AppCompatActivity() {
         robot.disconnect()
     }
 
-    private val blockHeight = 25.0
     fun move(view: View) {
-        val home = MoveToPoint(TypeOfMovement.LMOVE, robot.homePosition)
-        val takePoint = Position(-320.0,650.0,-270.0,90.0,180.0,0.0)
-        val putPoint = Position(170.0,540.0,-270.0,90.0,180.0,0.0)
-        val blockLength = 75.0
-        val d = (blockLength - 10) / 2
-
-        robot.run(kProgram {
-            for (i in 0..10){
-                add(takeAndPut(takePoint, putPoint, dx = d, dz = i*blockHeight))
-                add(takeAndPut(takePoint, putPoint, dx = -d, dz = i*blockHeight ))
-                add(takeAndPut(takePoint, putPoint, angle = 90.0, dy = d, dz = i*blockHeight))
-                add(takeAndPut(takePoint, putPoint, angle = 90.0,dy = -d, dz = i*blockHeight))
-            }
-            add(home)
-        })
+       ThirdProblem(robot).move()
     }
 
-    @ExecutedOnTheRobot
-    private fun takeAndPut(takePosition: Position, putPosition: Position, angle: Double = 0.0,
-                           dx: Double = 0.0, dy: Double = 0.0, dz: Double = 0.0) = kProgram {
-        add(takeBlock(takePosition))
-        add(putBlock(putPosition, dx = dx,dy = dy, dz = dz, angle = angle))
-    }
 
-    @ExecutedOnTheRobot
-    private fun takeBlock(takePosition: Position) = kProgram {
-        departPoint(position = takePosition, dZ = 100.0)
-        move(Coordinate.Z, -100.0)
-        closeGripper()
-        delay(2000L)
-        move(Coordinate.Z, 100.0)
-    }
-
-    @ExecutedOnTheRobot
-    private fun putBlock(putPosition: Position, angle: Double = 0.0,
-                         dx: Double = 0.0, dy: Double = 0.0, dz: Double = 0.0) = kProgram {
-        departPoint(position = putPosition,dX = dx, dY = dy,  dZ = 100.0 + dz, dT = angle)
-        move(Coordinate.Z, -100.0)
-        openGripper()
-        delay(2000L)
-        move(Coordinate.Z, blockHeight + 5)
-        move(Coordinate.X, -100.0)
-    }
 
     fun goHome(view: View) {
         robot.run(kProgram {
